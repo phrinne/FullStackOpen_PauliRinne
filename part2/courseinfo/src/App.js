6e079386cd2d5e19f375286ce7c600400ec3f45d
phrinne <75909538+phrinne@users.noreply.github.com>
2021-04-15 21:04:36 +0300

    Exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,45 +1,5 @@
 import React from 'react';
-
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Total = ({ course }) => {
-  const exercises = course.parts.map(part => part.exercises);
-  const sum = exercises.reduce((acc, cur) => acc + cur); 
-
-  return(
-    <strong>total of {sum} exercises</strong>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part part={part} key={part.id} />)}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
