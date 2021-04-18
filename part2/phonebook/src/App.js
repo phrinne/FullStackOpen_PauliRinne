7216278e4b6274c18187e492d303dcdaeb9af7f2
phrinne <75909538+phrinne@users.noreply.github.com>
2021-04-18 11:14:47 +0300

    Exercise 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -32,19 +32,15 @@ const Persons = ({persons}) => (
 )
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    /*{ name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }*/
-  ]) 
+  const [ persons, setPersons ] = useState( [] ) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterText, setFilterText ] = useState('')
+  const dbUrl = 'http://localhost:3001/persons'
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/persons')
+      .get(dbUrl)
       .then(response => setPersons(response.data))
   }, [])
 
@@ -58,9 +54,14 @@ const App = () => {
     }
 
     const person = { name: newName, number: newNumber }
-    setPersons(persons.concat(person))
-    setNewName('')
-    setNewNumber('')
+    axios
+    .post(dbUrl, person)
+    .then(response => {
+      setPersons(persons.concat(response.data))
+      setNewName('')
+      setNewNumber('')
+      console.log(response.data);
+    })
   }
 
   const handleNameChange = (e) => {
