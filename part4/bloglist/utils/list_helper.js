7ad06d51a7ee8cf4b997b9035c289f7fbf8cf6e3
phrinne <75909538+phrinne@users.noreply.github.com>
2021-04-25 15:11:38 +0300

    Exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.map(b => b.likes).reduce((total, sum) => total+sum, 0)
+    //return 5 blogs?0:
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
