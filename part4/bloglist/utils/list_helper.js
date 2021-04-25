1d5a095de2ee32850943c0b1c969e564c80a5361
phrinne <75909538+phrinne@users.noreply.github.com>
2021-04-25 15:32:25 +0300

    Exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,10 +4,29 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
     return blogs.map(b => b.likes).reduce((total, sum) => total+sum, 0)
-    //return 5 blogs?0:
+}
+
+const favoriteBlog = (blogs) => {
+    if(blogs.length === 0) {
+        return null
+    }
+    
+    let favorite = blogs[0]
+    for(let blog of blogs) {
+        if(blog.likes > favorite.likes) {
+            favorite = blog
+        }
+    }
+
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
