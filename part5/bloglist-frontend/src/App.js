86dc5bc3142c5977a3d6a265b5b0ebf45ec57958
phrinne <75909538+phrinne@users.noreply.github.com>
2021-05-03 20:53:28 +0300

    Exercise 5.3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -8,10 +8,13 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
 
   useEffect(() => {
-    blogService.getAll().then(blogs => {
-      setBlogs(blogs)
+    blogService.getAll().then(bs => {
+      setBlogs(bs)
     })
   }, [])
 
@@ -20,27 +23,50 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
-      //blogService.setToken(user.token)
+      blogService.setToken(user.token)
     }
   }, [])
+
+  const handleTitleChange = (event) => setNewTitle(event.target.value)
+  const handleAuthorChange = (event) => setNewAuthor(event.target.value)
+  const handleUrlChange = (event) => setNewUrl(event.target.value)
 
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
       setUser(user)
+      blogService.setToken(user.token)
       setUsername('')
       setPassword('')
       window.localStorage.setItem('loggedBlogsappUser', JSON.stringify(user))
     } catch (exception) {
-      /*setErrorMessage('Wrong credentials')
-      setTimeout(() => { setErrorMessage(null) }, 5000)*/
+      console.log(exception)
     }
   }
 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogsappUser')
     setUser(null)
+    blogService.setToken(null)
+  }
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    }
+    try {
+      const returnedBlog = await blogService.create(blogObject)
+      setBlogs( blogs.concat(returnedBlog) )
+      setNewTitle('')
+      setNewAuthor('')
+      setNewUrl('')
+    } catch (exception) {
+      console.log(exception)
+    }
   }
 
   if (user === null) {
@@ -62,6 +88,8 @@ const App = () => {
     )
   }
 
+  const blogsToShow = blogs.filter(b => b.user.username === user.username)
+
   return (
     <div>
       <h2>blogs</h2>
@@ -69,11 +97,24 @@ const App = () => {
         {user.name} logged-in
         <button type="button" onClick={handleLogout}>log out</button>
       </div>
-      <br/>
-      {blogs
-        .filter(blog => blog.user.username === user.username)
-        .map(blog => <Blog key={blog.id} blog={blog} />)
-      }
+
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          title:
+          <input value={newTitle} onChange={handleTitleChange} />
+        </div><div>
+          author:
+          <input value={newAuthor} onChange={handleAuthorChange} />
+        </div><div>
+          url:
+          <input value={newUrl} onChange={handleUrlChange} />
+        </div>
+        <button type="submit">create</button>
+      </form>
+      <br />
+
+      {blogsToShow.map(b => <Blog key={b.id} blog={b} />)}
     </div>
   )
 }
