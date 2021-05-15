7b07bf7cc24cffde0b8fb22c025bdb8376414eb1
phrinne <75909538+phrinne@users.noreply.github.com>
2021-05-15 20:08:00 +0300

    Exercise 5.18

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,4 +1,4 @@
-describe('Blog app', function() {
+/*describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
@@ -6,5 +6,39 @@ describe('Blog app', function() {
 
   it('Login form is shown', function() {
     cy.contains('Log in to application')
+  })
+})*/
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Testi Juuseri',
+      username: 'cypresshill',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('Log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('cypresshill')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Testi Juuseri logged-in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('cypresshill')
+      cy.get('#password').type('väärä')
+      cy.get('#login-button').click()
+
+      cy.contains('login failed')
+    })
   })
 })
