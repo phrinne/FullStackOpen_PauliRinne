dccaf3d508868382af8e324a2823e7d240cd1cb5
phrinne <75909538+phrinne@users.noreply.github.com>
2021-05-24 11:23:43 +0300

    Exercises 7.1-7.3

---
+++ part7/routed-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+const AnecdoteList = ({ anecdotes }) => (
+    <div>
+        <h2>Anecdotes</h2>
+        <ul>
+            {anecdotes.map(a => 
+                <li key={a.id}>
+                    <Link to={`/anecdotes/${a.id}`}>{a.content}</Link>
+                </li>)}
+        </ul>
+    </div>
+)
+
+export default AnecdoteList
---
+++ part7/routed-anecdotes/src/components/Notification.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = ({ notification }) => (
+  <div>
+      {notification}
+  </div>
+)
+
+export default Notification
