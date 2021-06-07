dec2dbdd57b5ae40dec63d6831682d6426671282
phrinne <75909538+phrinne@users.noreply.github.com>
2021-06-07 20:08:30 +0300

    Exercise 8.5

--- part8/library/library-backend.js
+++ part8/library/library-backend.js
@@ -107,7 +107,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -117,10 +117,16 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books
       }
-      return books.filter(b => b.author === args.author)
+      if(!args.author) {
+        return books.filter(b => b.genres.includes(args.genre))
+      }
+      if(!args.genre) {
+        return books.filter(b => b.author === args.author)
+      } 
+      return books.filter(b => b.author === args.author && b.genres.includes(args.genre))
     },
     allAuthors: () => authors
   },
