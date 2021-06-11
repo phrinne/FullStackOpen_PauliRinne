aa66e6bf71763a9d8aec5bb8be45996d1d3060c0
phrinne <75909538+phrinne@users.noreply.github.com>
2021-06-11 19:16:51 +0300

    Exercise 9.9

--- part9/patientor_backend/build/index.js
+++ part9/patientor_backend/build/index.js
@@ -10,7 +10,7 @@ app.get('/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
-const PORT = 3003;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- part9/patientor_backend/index.ts
+++ part9/patientor_backend/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
 
-const PORT = 3003;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
