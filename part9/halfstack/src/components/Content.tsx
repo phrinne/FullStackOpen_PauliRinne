ff281e77d86e4220993ce5e55f770394ccdc675b
phrinne <75909538+phrinne@users.noreply.github.com>
2021-06-16 11:52:16 +0300

    patch

--- part9/halfstack/src/components/Content.tsx
+++ part9/halfstack/src/components/Content.tsx
@@ -2,21 +2,6 @@ import React from 'react';
 import { CoursePart } from '../App'
 import Part from './Part';
 
-/*interface ContentProps {
-  courseParts: Array<{name: string, exerciseCount: number}>;
-}*/
-
-/*const Content = ({ courseParts }: ContentProps) => (
-  <>
-    {courseParts.map(p => <Part part={p} key={p.name} />)}
-  </>
-)*/
-
-/*const Content = ({ courseParts }: { courseParts: Array<CoursePart> }) => (
-  <>
-    {courseParts.map(p => <p key={p.name}>{p.name} {p.exerciseCount}</p>)}
-  </>
-)*/
 const Content = ({ courseParts }: { courseParts: Array<CoursePart> }) => (
   <>
     {courseParts.map(p => <Part key={p.name} part={p} />)}
